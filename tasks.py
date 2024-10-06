f2b7127bbef7e02247a9cf01e3424d934d30c655
Robin Ignacio <105887696+greatdecipher@users.noreply.github.com>
2024-10-06 23:34:28 +0800

    remove other disables

--- tasks.py
+++ tasks.py
@@ -51,9 +51,9 @@ class ThoughfulScraper:
             options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--lang=en-US')
-        options.add_argument("--disable-extensions")
-        options.add_argument("--disable-gpu")
-        options.add_argument('--disable-web-security')
+        # options.add_argument("--disable-extensions")
+        # options.add_argument("--disable-gpu")
+        # options.add_argument('--disable-web-security')
         options.add_argument("--start-maximized")
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
         return options
